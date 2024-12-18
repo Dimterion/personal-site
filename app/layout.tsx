77b933e81b6dec6cc7d9d7f0b8e617ed2c9e3c56
Dmitrii <dimterion59@gmail.com>
2024-12-18 10:37:34 +0100

    adjust scroll-btn style

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Providers from "@/components/providers";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import ScrollBtn from "@/components/scroll-btn";
 import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
@@ -36,6 +37,7 @@ export default function RootLayout({
           <Header />
           <main className="grow">{children}</main>
           <Footer />
+          <ScrollBtn />
         </Providers>
       </body>
     </html>
