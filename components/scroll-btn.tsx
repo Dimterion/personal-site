14669cbf837f23bbe4a12f99388ba0d1ec43d8ac
Dmitrii <dimterion59@gmail.com>
2024-12-18 10:25:04 +0100

    add scroll-btn comp

---
+++ components/scroll-btn.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { FaArrowUp } from "react-icons/fa6";
+
+function ScrollBtn() {
+  const [showScrollBtn, setShowScrollBtn] = useState(false);
+
+  function scrollToTop() {
+    window.scrollTo(0, 0);
+  }
+
+  function handleScroll() {
+    setShowScrollBtn(window.scrollY > 0);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <button
+      className={`fixed bottom-10 right-3 z-10 flex size-6 items-center justify-center rounded-lg bg-gray-500 font-bold text-gray-200 opacity-50 shadow-lg hover:opacity-100 ${
+        showScrollBtn ? "block" : "hidden"
+      }`}
+      aria-label="Scroll to top"
+      onClick={() => scrollToTop()}
+    >
+      <FaArrowUp />
+    </button>
+  );
+}
+
+export default ScrollBtn;
