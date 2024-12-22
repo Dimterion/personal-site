b74e1acdf116021ec5571a18d538ac1f1cf147d9
Dmitrii <dimterion59@gmail.com>
2024-12-22 10:54:23 +0100

    add NewsletterForm to PostPage

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,15 +1,20 @@
 import { getPosts } from "@/lib/posts";
 import PostsWithSearch from "@/components/posts-with-search";
+import NewsletterForm from "@/components/newsletter-form";
 
 export default async function PostPage() {
   const posts = await getPosts();
 
   return (
     <section className="pb-24 pt-40">
-      <div className="container max-w-3xl">
+      <article className="container max-w-3xl">
         <h1 className="title mb-12">Posts</h1>
         <PostsWithSearch posts={posts} />
-      </div>
+        <hr className="mt-12"></hr>
+      </article>
+      <article className="container mt-12 max-w-3xl">
+        <NewsletterForm />
+      </article>
     </section>
   );
 }
