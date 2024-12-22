b58578e5dda26c843fe49907808add954852b7b7
Dmitrii <dimterion59@gmail.com>
2024-12-22 11:55:37 +0100

    add profile-link comp

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,4 +1,5 @@
 import Projects from "@/components/projects";
+import ProfileLink from "@/components/profile-link";
 import { getProjects } from "@/lib/projects";
 
 export default async function ProjectsPage() {
@@ -9,7 +10,16 @@ export default async function ProjectsPage() {
       <div className="container max-w-3xl">
         <h1 className="title mb-12">Projects</h1>
         <Projects projects={projects} />
+        <hr className="mt-12"></hr>
       </div>
+      <article className="container mt-12 max-w-3xl">
+        <ProfileLink
+          heading="View my GitHub profile"
+          text="I code every day and keep most of my repos open."
+          link="https://github.com/Dimterion"
+          linkText="Visit my GitHub"
+        />
+      </article>
     </section>
   );
 }
---
+++ components/profile-link.tsx
@@ -0,0 +1,39 @@
+import { ExternalLinkIcon } from "@radix-ui/react-icons";
+import { Card, CardContent } from "@/components/ui/card";
+
+export default function ProfileLink({
+  heading,
+  text,
+  link,
+  linkText,
+}: {
+  heading: string;
+  text: string;
+  link: string;
+  linkText: string;
+}) {
+  return (
+    <section>
+      <Card className="rounded-lg border-0 dark:border">
+        <CardContent className="flex flex-col gap-8 pt-6 md:flex-row md:items-center md:justify-between">
+          <div>
+            <h2 className="text-2xl font-bold">{heading}</h2>
+            <p className="text-muted-foreground">{text}</p>
+          </div>
+          <div>
+            <div className="w-full font-bold">@Dimterion</div>
+            <a
+              href={link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-flex items-center gap-1 text-muted-foreground underline underline-offset-4 transition-colors hover:text-foreground"
+            >
+              {linkText}
+              <ExternalLinkIcon />
+            </a>
+          </div>
+        </CardContent>
+      </Card>
+    </section>
+  );
+}
