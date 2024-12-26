8c4272b2319c9788fcc1a18f858600d3a3867584
Dmitrii <dimterion59@gmail.com>
2024-12-26 11:12:04 +0100

    adjust home & not-found pages

--- app/page.tsx
+++ app/page.tsx
@@ -5,18 +5,16 @@ import ProfileLink from "@/components/profile-link";
 
 export default function Home() {
   return (
-    <section className="py-24">
-      <div className="container max-w-3xl">
-        <Intro />
-        <RecentProjects />
-        <RecentPosts />
-        <ProfileLink
-          heading="Read my stories on Medium"
-          text="I write every Friday and share what I work on and learn."
-          link="https://medium.com/@dimterion"
-          linkText="Visit my blog"
-        />
-      </div>
+    <section className="container max-w-3xl py-24">
+      <Intro />
+      <RecentProjects />
+      <RecentPosts />
+      <ProfileLink
+        heading="Read my stories on Medium"
+        text="I write every Friday and share what I work on and learn."
+        link="https://medium.com/@dimterion"
+        linkText="Visit my blog"
+      />
     </section>
   );
 }
