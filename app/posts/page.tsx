5b5ac2a4c603a35cc08bf188851b8280db80121b
Dmitrii <dimterion59@gmail.com>
2024-12-27 11:52:21 +0100

    adjust custom css, projects & posts pages

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,25 +1,21 @@
-import { getPosts } from "@/lib/posts";
 import PostsWithSearch from "@/components/posts-with-search";
 import ProfileLink from "@/components/profile-link";
+import { getPosts } from "@/lib/posts";
 
 export default async function PostPage() {
   const posts = await getPosts();
 
   return (
-    <section className="pb-24 pt-40">
-      <article className="container max-w-3xl">
-        <h1 className="title mb-12">Posts</h1>
-        <PostsWithSearch posts={posts} />
-        <hr className="mt-12"></hr>
-      </article>
-      <article className="container mt-12 max-w-3xl">
-        <ProfileLink
-          heading="Read my stories on Medium"
-          text="I write every Friday and share what I work on and learn."
-          link="https://medium.com/@dimterion"
-          linkText="Visit my blog"
-        />
-      </article>
+    <section className="container max-w-3xl pb-24 pt-40">
+      <h1 className="title mb-12">Posts</h1>
+      <PostsWithSearch posts={posts} />
+      <hr className="my-12"></hr>
+      <ProfileLink
+        heading="Read my stories on Medium"
+        text="I write every Friday and share what I work on and learn."
+        link="https://medium.com/@dimterion"
+        linkText="Visit my blog"
+      />
     </section>
   );
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -6,20 +6,16 @@ export default async function ProjectsPage() {
   const projects = await getProjects();
 
   return (
-    <section className="pb-24 pt-40">
-      <div className="container max-w-3xl">
-        <h1 className="title mb-12">Projects</h1>
-        <Projects projects={projects} />
-        <hr className="mt-12"></hr>
-      </div>
-      <article className="container mt-12 max-w-3xl">
-        <ProfileLink
-          heading="View my GitHub profile"
-          text="I code every day and keep most of my repos open."
-          link="https://github.com/Dimterion"
-          linkText="Visit my GitHub"
-        />
-      </article>
+    <section className="container max-w-3xl pb-24 pt-40">
+      <h1 className="title mb-12">Projects</h1>
+      <Projects projects={projects} />
+      <hr className="my-12"></hr>
+      <ProfileLink
+        heading="View my GitHub profile"
+        text="I code every day and keep most of my repos open."
+        link="https://github.com/Dimterion"
+        linkText="Visit my GitHub"
+      />
     </section>
   );
 }
