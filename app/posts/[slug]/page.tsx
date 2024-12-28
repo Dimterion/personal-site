d0fe6791a093a225b3d8ddec7908311670563c65
Dmitrii <dimterion59@gmail.com>
2024-12-28 10:05:52 +0100

    adjust post page structure

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,11 +1,11 @@
+import Link from "next/link";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import { ArrowLeftIcon } from "@radix-ui/react-icons";
 import MDXContent from "@/components/mdx-content";
 import ProfileLink from "@/components/profile-link";
-import { getPostBySlug, getPosts } from "@/lib/posts";
 import { formatDate } from "@/lib/utils";
-import { ArrowLeftIcon } from "@radix-ui/react-icons";
-import Image from "next/image";
-import Link from "next/link";
-import { notFound } from "next/navigation";
+import { getPostBySlug, getPosts } from "@/lib/posts";
 
 export async function generateStaticParams() {
   const posts = await getPosts();
@@ -28,44 +28,42 @@ export default async function Post({ params }: { params: { slug: string } }) {
   const { title, image, author, publishedAt } = metadata;
 
   return (
-    <section className="pb-24 pt-32">
-      <div className="container max-w-3xl">
-        <Link
-          href="/posts"
-          className="mb-8 inline-flex items-center gap-2 text-center text-sm font-light text-zinc-500 transition-colors hover:text-foreground dark:text-zinc-400 dark:hover:text-foreground"
-        >
-          <ArrowLeftIcon className="h-5 w-5" />
-          <span>View all posts</span>
-        </Link>
-        {image && (
-          <div className="relative mb-6 h-52 w-full overflow-hidden rounded-lg sm:h-96">
-            <Image
-              src={image}
-              alt={title || ""}
-              className="object-cover"
-              fill
-              sizes="96"
-              priority
-            />
-          </div>
-        )}
-        <header>
-          <h1 className="title">{title}</h1>
-          <p className="mt-3 text-xs text-muted-foreground">
-            {author} / {formatDate(publishedAt ?? "")}
-          </p>
-        </header>
-        <main className="prose mt-16 dark:prose-invert">
-          <MDXContent source={content} />
-        </main>
-        <hr className="my-10"></hr>
-        <ProfileLink
-          heading="Read my stories on Medium"
-          text="I write every Friday and share what I work on and learn."
-          link="https://medium.com/@dimterion"
-          linkText="Visit my blog"
-        />
-      </div>
+    <section className="container max-w-3xl pb-24 pt-32">
+      <Link
+        href="/posts"
+        className="mb-8 inline-flex items-center gap-2 text-center text-sm font-light text-zinc-500 transition-colors hover:text-foreground dark:text-zinc-400 dark:hover:text-foreground"
+      >
+        <ArrowLeftIcon className="h-5 w-5" />
+        <span>View all posts</span>
+      </Link>
+      {image && (
+        <div className="relative mb-6 h-52 w-full overflow-hidden rounded-lg sm:h-96">
+          <Image
+            src={image}
+            alt={title || ""}
+            className="object-cover"
+            fill
+            sizes="96"
+            priority
+          />
+        </div>
+      )}
+      <aside>
+        <h1 className="title">{title}</h1>
+        <p className="mt-3 text-xs text-muted-foreground">
+          {author} / {formatDate(publishedAt ?? "")}
+        </p>
+      </aside>
+      <main className="prose mt-16 dark:prose-invert">
+        <MDXContent source={content} />
+      </main>
+      <hr className="my-10"></hr>
+      <ProfileLink
+        heading="Read my stories on Medium"
+        text="I write every Friday and share what I work on and learn."
+        link="https://medium.com/@dimterion"
+        linkText="Visit my blog"
+      />
     </section>
   );
 }
