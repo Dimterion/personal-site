9614b6b7c70b082a73ec91b2fb50c827fb3245ef
Dmitrii <dimterion59@gmail.com>
2024-12-28 09:51:23 +0100

    adjust project page structure

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,11 +1,11 @@
+import Link from "next/link";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import { ArrowLeftIcon } from "@radix-ui/react-icons";
 import MDXContent from "@/components/mdx-content";
 import ProfileLink from "@/components/profile-link";
-import { getProjectBySlug, getProjects } from "@/lib/projects";
 import { formatDate } from "@/lib/utils";
-import { ArrowLeftIcon } from "@radix-ui/react-icons";
-import Image from "next/image";
-import Link from "next/link";
-import { notFound } from "next/navigation";
+import { getProjectBySlug, getProjects } from "@/lib/projects";
 
 export async function generateStaticParams() {
   const projects = await getProjects();
@@ -32,44 +32,42 @@ export default async function Project({
   const { title, image, author, publishedAt } = metadata;
 
   return (
-    <section className="pb-24 pt-32">
-      <div className="container max-w-3xl">
-        <Link
-          href="/projects"
-          className="mb-8 inline-flex items-center gap-2 text-center text-sm font-light text-zinc-500 transition-colors hover:text-foreground dark:text-zinc-400 dark:hover:text-foreground"
-        >
-          <ArrowLeftIcon className="h-5 w-5" />
-          <span>View all projects</span>
-        </Link>
-        {image && (
-          <div className="relative mb-6 h-52 w-full overflow-hidden rounded-lg sm:h-96">
-            <Image
-              src={image}
-              alt={title || ""}
-              className="object-cover"
-              fill
-              sizes="96"
-              priority
-            />
-          </div>
-        )}
-        <header>
-          <h1 className="title">{title}</h1>
-          <p className="mt-3 text-xs text-muted-foreground">
-            {author} / {formatDate(publishedAt ?? "")}
-          </p>
-        </header>
-        <main className="prose mt-16 dark:prose-invert">
-          <MDXContent source={content} />
-        </main>
-        <hr className="my-10"></hr>
-        <ProfileLink
-          heading="View my GitHub profile"
-          text="I code every day and keep most of my repos open."
-          link="https://github.com/Dimterion"
-          linkText="Visit my GitHub"
-        />
-      </div>
+    <section className="container max-w-3xl pb-24 pt-32">
+      <Link
+        href="/projects"
+        className="mb-8 inline-flex items-center gap-2 text-center text-sm font-light text-zinc-500 transition-colors hover:text-foreground dark:text-zinc-400 dark:hover:text-foreground"
+      >
+        <ArrowLeftIcon className="h-5 w-5" />
+        <span>View all projects</span>
+      </Link>
+      {image && (
+        <div className="relative mb-6 h-52 w-full overflow-hidden rounded-lg sm:h-96">
+          <Image
+            src={image}
+            alt={title || ""}
+            className="object-cover"
+            fill
+            sizes="96"
+            priority
+          />
+        </div>
+      )}
+      <aside>
+        <h1 className="title">{title}</h1>
+        <p className="mt-3 text-xs text-muted-foreground">
+          {author} / {formatDate(publishedAt ?? "")}
+        </p>
+      </aside>
+      <main className="prose mt-16 dark:prose-invert">
+        <MDXContent source={content} />
+      </main>
+      <hr className="my-10"></hr>
+      <ProfileLink
+        heading="View my GitHub profile"
+        text="I code every day and keep most of my repos open."
+        link="https://github.com/Dimterion"
+        linkText="Visit my GitHub"
+      />
     </section>
   );
 }
