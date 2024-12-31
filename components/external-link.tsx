8c10ac3de75e932fee3075b3af5b95ce2cb7d56d
Dmitrii <dimterion59@gmail.com>
2024-12-31 12:15:05 +0100

    add external-link comp

---
+++ components/external-link.tsx
@@ -0,0 +1,20 @@
+import { ExternalLinkIcon } from "@radix-ui/react-icons";
+
+export default function ExternalLink({
+  link,
+  name,
+}: {
+  link: string;
+  name: string;
+}) {
+  return (
+    <a
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="inline-flex items-center gap-1 text-sm underline underline-offset-4 transition-colors hover:text-foreground sm:text-base"
+    >
+      {name} <ExternalLinkIcon />
+    </a>
+  );
+}
