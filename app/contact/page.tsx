965f2290f8b51aa84478c09b17cf8d291f594896
Dmitrii <dimterion59@gmail.com>
2025-01-01 11:09:13 +0100

    change email link

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,15 +1,18 @@
 import ContactForm from "@/components/contact-form";
-import EmailLink from "@/components/email-link";
 import ExternalLink from "@/components/external-link";
 
 export default function Contact() {
+  const email = atob(process.env.NEXT_PUBLIC_EMAIL_KEY as string);
+
   return (
     <section className="container max-w-3xl pb-24 pt-40">
       <h2 className="title">Contact form</h2>
       <ContactForm />
       <hr className="mt-8"></hr>
       <article className="flex flex-row flex-wrap items-baseline justify-between">
-        <EmailLink />
+        <section className="mt-8">
+          Contact me by <ExternalLink link={`mailto:${email}`} name="email" />
+        </section>
         <section className="mt-8">
           View my{" "}
           <ExternalLink
