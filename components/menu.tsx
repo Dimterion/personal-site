f8f611ca477f5eea8abd2b7f793037d4ba6fa0e0
Dmitrii <dimterion59@gmail.com>
2025-01-02 11:17:05 +0100

    add menu component

---
+++ components/menu.tsx
@@ -0,0 +1,17 @@
+import ExternalLink from "./external-link";
+
+export default function Menu() {
+  return (
+    <details className="relative">
+      <summary className="cursor-pointer text-sm text-muted-foreground transition-colors hover:text-foreground sm:text-base">
+        Resume
+      </summary>
+      <div className="absolute left-0 top-[100%] z-10 mt-2 flex w-36 flex-col">
+        <ExternalLink
+          link="/documents/Resume_Dmitrii_Popov_public.pdf"
+          name="Resume (pdf)"
+        />
+      </div>
+    </details>
+  );
+}
