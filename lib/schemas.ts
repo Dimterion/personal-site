fc43edf93c617dbe532716d9d5f7e33b0f2dca73
Dmitrii <dimterion59@gmail.com>
2025-02-02 12:21:23 +0100

    adjust form validation

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -3,13 +3,19 @@ import { z } from "zod";
 export const ContactFormSchema = z.object({
   name: z
     .string()
+    .trim()
     .min(1, { message: "Name is required." })
     .min(2, { message: "Two characters minimum." }),
   email: z
     .string()
+    .trim()
     .min(1, { message: "Email is required." })
     .email("Invalid email."),
-  message: z.string().min(1, { message: "Message is required." }),
+  message: z
+    .string()
+    .trim()
+    .min(1, { message: "Message is required." })
+    .min(5, { message: "Five  characters minimum." }),
   access_key: z.string(),
   subject: z.string(),
   from_name: z.string(),
