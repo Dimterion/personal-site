3839bfc68cf5a84e475c9bc1722a75e5f7ac70b2
Dmitrii <dimterion59@gmail.com>
2025-08-29 11:08:52 +0200

    adjust medium-posts elements structure

--- components/medium-posts.tsx
+++ components/medium-posts.tsx
@@ -31,20 +31,20 @@ export default async function MediumPosts() {
   }
 
   return (
-    <ul className="flex flex-col gap-8">
+    <ul className="flex flex-col gap-4">
       {posts.map((post) => (
         <li
           key={post.guid}
-          className="flex flex-col justify-between gap-x-4 gap-y-1 sm:flex-row"
+          className="flex min-h-44 flex-col justify-between gap-x-4 gap-y-1 rounded-md border p-6 sm:flex-row sm:items-center"
         >
-          <article className="max-w-lg">
+          <article className="flex max-w-lg flex-col flex-nowrap gap-1">
             <p className="text-lg font-semibold">{post.title}</p>
             <a
               href={post.link}
               target="_blank"
               rel="noopener noreferrer"
               aria-label={post.title}
-              className="mt-1 line-clamp-2 inline-flex items-center gap-1 font-light text-muted-foreground underline hover:text-foreground"
+              className="mt-1 line-clamp-2 inline-flex items-center gap-1 text-sm font-light text-muted-foreground underline hover:text-foreground sm:text-base"
             >
               Read this post on Medium <ExternalLinkIcon />
             </a>
@@ -61,7 +61,7 @@ export default async function MediumPosts() {
             </aside>
           </article>
           {post.pubDate && (
-            <p className="mt-1 text-sm font-light">
+            <p className="mt-4 text-sm font-light sm:mt-0">
               {formatDate(post.pubDate)}
             </p>
           )}
