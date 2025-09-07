9b29543f94a0286e0fbd5afe44a159488c93304e
Dmitrii <dimterion59@gmail.com>
2025-09-07 09:20:56 +0200

    add medium posts pagination

--- app/medium-posts/page.tsx
+++ app/medium-posts/page.tsx
@@ -11,20 +11,26 @@ export const metadata: Metadata = {
   title: "Medium posts",
 };
 
-export default function MediumPostsPage() {
+export default function MediumPostsPage({
+  searchParams,
+}: {
+  searchParams: { page?: string };
+}) {
+  const page = parseInt(searchParams.page ?? "1", 10);
+
   return (
     <section className="container max-w-3xl pb-24 pt-40">
       <h1 className="title mb-12">Latest Medium Posts</h1>
-      <Suspense fallback={<Skeleton />}>
-        <MediumPosts />
-      </Suspense>
       <Link
         href="/posts"
-        className="mt-12 inline-flex items-center gap-2 text-center text-sm font-light text-zinc-500 transition-colors hover:text-foreground dark:text-zinc-400 dark:hover:text-foreground"
+        className="mb-12 inline-flex items-center gap-2 text-center text-sm font-light text-zinc-500 transition-colors hover:text-foreground dark:text-zinc-400 dark:hover:text-foreground"
       >
         <ArrowLeftIcon className="h-5 w-5" />
         <span>Back to blog posts</span>
       </Link>
+      <Suspense fallback={<Skeleton />}>
+        <MediumPosts page={page} />
+      </Suspense>
       <hr className="my-12"></hr>
       <ProfileLink
         heading={defaultMediumLinkContent.heading}
--- components/medium-posts.tsx
+++ components/medium-posts.tsx
@@ -1,9 +1,14 @@
 import Link from "next/link";
-import { ExternalLinkIcon } from "@radix-ui/react-icons";
+import {
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  ExternalLinkIcon,
+} from "@radix-ui/react-icons";
 import { getMediumPosts, MediumPostMetadata } from "@/lib/medium-posts";
 import { extractFirstSentence, formatDate } from "@/lib/utils";
 
-export default async function MediumPosts() {
+export default async function MediumPosts({ page = 1 }: { page: number }) {
+  const POSTS_PER_PAGE = 5;
   let posts: MediumPostMetadata[] = [];
   let postsLoadingError = false;
 
@@ -30,46 +35,74 @@ export default async function MediumPosts() {
     );
   }
 
+  const totalPages = Math.ceil(posts.length / POSTS_PER_PAGE);
+  const paginatedPosts = posts.slice(
+    (page - 1) * POSTS_PER_PAGE,
+    page * POSTS_PER_PAGE,
+  );
+
   return (
-    <ul className="flex flex-col gap-4">
-      {posts.map((post) => (
-        <li
-          key={post.guid}
-          className="flex min-h-56 flex-col justify-between gap-x-4 gap-y-1 rounded-md border p-6 sm:flex-row sm:items-center"
-        >
-          <article className="flex max-w-lg flex-col flex-nowrap gap-1">
-            <p className="text-lg font-semibold">{post.title}</p>
-            <p className="mt-1 line-clamp-2 text-sm font-light text-muted-foreground">
-              {extractFirstSentence(post.description)}
-            </p>
-            <a
-              href={post.link}
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label={post.title}
-              className="mt-1 line-clamp-2 inline-flex items-center gap-1 text-sm text-muted-foreground underline hover:text-foreground sm:text-base"
-            >
-              Read this post on Medium <ExternalLinkIcon />
-            </a>
-            <aside className="mt-1 flex flex-wrap">
-              {post.categories &&
-                post.categories.map((category, index) => (
-                  <pre
-                    key={`${category}-${index}`}
-                    className="mr-2 mt-2 w-fit rounded-lg border bg-muted p-1 text-xs font-semibold text-foreground"
-                  >
-                    {category}
-                  </pre>
-                ))}
-            </aside>
-          </article>
-          {post.pubDate && (
-            <p className="mt-4 text-sm font-light sm:mt-0">
-              {formatDate(post.pubDate)}
-            </p>
-          )}
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className="flex flex-col gap-4">
+        {paginatedPosts.map((post) => (
+          <li
+            key={post.guid}
+            className="flex min-h-56 flex-col justify-between gap-x-4 gap-y-1 rounded-md border p-6 sm:flex-row sm:items-center"
+          >
+            <article className="flex max-w-lg flex-col flex-nowrap gap-1">
+              <p className="text-lg font-semibold">{post.title}</p>
+              <p className="mt-1 line-clamp-2 text-sm font-light text-muted-foreground">
+                {extractFirstSentence(post.description)}
+              </p>
+              <a
+                href={post.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label={post.title}
+                className="mt-1 line-clamp-2 inline-flex items-center gap-1 text-sm text-muted-foreground underline hover:text-foreground sm:text-base"
+              >
+                Read this post on Medium <ExternalLinkIcon />
+              </a>
+              <aside className="mt-1 flex flex-wrap">
+                {post.categories &&
+                  post.categories.map((category, index) => (
+                    <pre
+                      key={`${category}-${index}`}
+                      className="mr-2 mt-2 w-fit rounded-lg border bg-muted p-1 text-xs font-semibold text-foreground"
+                    >
+                      {category}
+                    </pre>
+                  ))}
+              </aside>
+            </article>
+            {post.pubDate && (
+              <p className="mt-4 text-sm font-light sm:mt-0">
+                {formatDate(post.pubDate)}
+              </p>
+            )}
+          </li>
+        ))}
+      </ul>
+      <nav className="mt-8 flex justify-center gap-4">
+        {page > 1 && (
+          <Link
+            href={`?page=${page - 1}`}
+            className="inline-flex items-center gap-2 text-center text-sm font-light text-zinc-500 transition-colors hover:text-foreground dark:text-zinc-400 dark:hover:text-foreground"
+          >
+            <ArrowLeftIcon className="h-5 w-5" />
+            <span>Previous posts</span>
+          </Link>
+        )}
+        {page < totalPages && (
+          <Link
+            href={`?page=${page + 1}`}
+            className="inline-flex items-center gap-2 text-center text-sm font-light text-zinc-500 transition-colors hover:text-foreground dark:text-zinc-400 dark:hover:text-foreground"
+          >
+            <span>Next posts</span>
+            <ArrowRightIcon className="h-5 w-5" />
+          </Link>
+        )}
+      </nav>
+    </>
   );
 }
