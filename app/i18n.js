9b89ad7842abf6b3d6ad07e13a3431d11db921c6
Dmitrii <dimterion59@gmail.com>
2026-01-18 12:24:28 +0100

    add initial i18n setup

---
+++ app/i18n.js
@@ -0,0 +1,28 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import enTranslations from "./locales/en/translation.json";
+import frTranslations from "./locales/fr/translation.json";
+
+const resources = {
+  en: {
+    translation: enTranslations,
+  },
+  fr: {
+    translation: frTranslations,
+  },
+};
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: "en",
+    defaultNS: "translation",
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
